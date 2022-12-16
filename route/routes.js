55a754418cd2732fc00b173dac303ecf65559a95
naythankik <dolapokiki3ird@gmail.com>
2022-12-16 10:35:28 +0100

    modify directories

--- route/routes.js
+++ route/routes.js
@@ -1,89 +1,87 @@
 const express = require("express");
 const Joi = require("joi");
-const users = require("../public/users");
+const users = require("../public/assets/js/users");
 
 const routers = express.Router();
 
 // The validation for user inputs
 function validation(param) {
-    let schema = Joi.object({
-        firstName: Joi.string().min(3).required(),
-        lastName: Joi.string().min(3).required(),
-        username: Joi.string().min(3).required(),
-        telephone: Joi.number().min(11).required(),
-        address: Joi.string().min(3).required(),
-        password: Joi.string().min(6).required(),
-    });
-    let err = schema.validate(param);
-    if (err.error) {
-        return err.error.details[0].message;
-    }
+  let schema = Joi.object({
+    firstName: Joi.string().min(3).required(),
+    lastName: Joi.string().min(3).required(),
+    username: Joi.string().min(3).required(),
+    telephone: Joi.number().min(11).required(),
+    address: Joi.string().min(3).required(),
+    password: Joi.string().min(6).required(),
+  });
+  let err = schema.validate(param);
+  if (err.error) {
+    return err.error.details[0].message;
+  }
 }
 
 // The user controller routes
 routers
-    .route("/")
-    .get((req, res) => {
-        res.send(users);
-        // res.send({ message: "Welcome to VIDAPP, Signin" });
-    })
-    .post((req, res) => {
-        if (validation(req.body)) {
-            res.status(403).send(validation(req.body));
-        }
-        req.body.id = users.length + 1;
-        users.push(req.body);
-        res.status(200).send("The user has been registered successfully!");
-    });
+  .route("/")
+  .get((req, res) => {
+    res.send(users);
+    // res.send({ message: "Welcome to VIDAPP, Signin" });
+  })
+  .post((req, res) => {
+    if (validation(req.body)) {
+      res.status(403).send(validation(req.body));
+    }
+    req.body.id = users.length + 1;
+    users.push(req.body);
+    res.status(200).send("The user has been registered successfully!");
+  });
 
 routers
-    .route("/:id")
-    // fetching a single user
-    .get((req, res) => {
-        const user = users.find((val) => val.id === parseInt(req.params.id));
-        if (!user) {
-            res.status(403).send({
-                warning: `The user with the id, ${req.params.id} is not found`,
-            });
-        } else {
-            res.status(200).send(user);
-        }
-    })
-    //The updating user
-    .put((req, res) => {
-        const updUser = users.find((val) => val.id === parseInt(req.params.id));
+  .route("/:id")
+  // fetching a single user
+  .get((req, res) => {
+    const user = users.find((val) => val.id === parseInt(req.params.id));
+    if (!user) {
+      res.status(403).send({
+        warning: `The user with the id, ${req.params.id} is not found`,
+      });
+    } else {
+      res.status(200).send(user);
+    }
+  })
+  //The updating user
+  .put((req, res) => {
+    const updUser = users.find((val) => val.id === parseInt(req.params.id));
 
-        if (!updUser) {
-            res.status(403).send({
-                warning: `The user with the id, ${req.params.id} is not found`,
-            });
-        } else {
-            (updUser.firstName = req.body.firstName || updUser.firstName),
-                (updUser.lastName = req.body.lastName || updUser.lastName),
-                (updUser.username = req.body.username || updUser.username),
-                (updUser.telephone = req.body.telephone || updUser.telephone),
-                (updUser.address = req.body.address || updUser.address),
-                (updUser.password = req.body.password || updUser.password),
-                res.status(200).send({
-                    message: `The user with id ${req.params.id} has been updated!`,
-                });
-        }
-    })
-    .delete((req, res) => {
-        const del = users.findIndex(
-            (val) => val.id === parseInt(req.params.id)
-        );
+    if (!updUser) {
+      res.status(403).send({
+        warning: `The user with the id, ${req.params.id} is not found`,
+      });
+    } else {
+      (updUser.firstName = req.body.firstName || updUser.firstName),
+        (updUser.lastName = req.body.lastName || updUser.lastName),
+        (updUser.username = req.body.username || updUser.username),
+        (updUser.telephone = req.body.telephone || updUser.telephone),
+        (updUser.address = req.body.address || updUser.address),
+        (updUser.password = req.body.password || updUser.password),
+        res.status(200).send({
+          message: `The user with id ${req.params.id} has been updated!`,
+        });
+    }
+  })
+  .delete((req, res) => {
+    const del = users.findIndex((val) => val.id === parseInt(req.params.id));
 
-        if (del < 0) {
-            res.status(403).send({
-                warning: `The user with the id, ${req.params.id} is not found`,
-            });
-        } else {
-            users.splice(del, 1);
-            res.status(200).json({
-                warning: `The user with the id, ${req.params.id} has been deleted`,
-            });
-        }
-    });
+    if (del < 0) {
+      res.status(403).send({
+        warning: `The user with the id, ${req.params.id} is not found`,
+      });
+    } else {
+      users.splice(del, 1);
+      res.status(200).json({
+        warning: `The user with the id, ${req.params.id} has been deleted`,
+      });
+    }
+  });
 
 module.exports = routers;
